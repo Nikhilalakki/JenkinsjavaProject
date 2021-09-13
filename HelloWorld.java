5224028288bc3f215cdedebb2b8c4f3b49684a4c
lakkireddy nikhila <lakkinikhila7@gmail.com>
2021-09-13 10:52:24 +0530

    added java code for jenkins

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String [] s){
+        System.out.println("Hello IBM");
+        for(int i=0;i<=10;i++){
+            System.out.println("Value of I: "+i);
+            
+        }
+    }
+}
